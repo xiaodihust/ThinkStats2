1e6e9430c37f743df1ceb7527033916c779bd5d1
xiaodihust <xiaodihust@hotmail.com>
2017-07-29 11:06:09 +0800

    Update 20170729

--- code/matt_df.py
+++ code/matt_df.py
@@ -1,3 +1,17 @@
-# Author: Matt
-# Get familiar with data frame in Think Stats
+# Author: Matt Xiao
+# Data frame from ThinkStats
+
+import nsfg
+import thinkstats2
+import thinkplot
+
+df = nsfg.ReadFemPreg()
+
+
+hist = thinkstats2.Hist([1, 2, 2, 3, 5])
+
+thinkplot.Hist(hist)
+thinkplot.Show(xlabel='value', ylabel='frequency')
+
+
 
